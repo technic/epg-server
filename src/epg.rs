7f68aac5c999499aa3458706f8449f2edd81d883
Alex Maystrenko <alexeytech@gmail.com>
2019-10-05 17:23:10 +0200

    cargo fmt

--- src/epg.rs
+++ src/epg.rs
@@ -1,5 +1,5 @@
 use chrono::prelude::*;
-use serde_derive::{Serialize, Deserialize};
+use serde_derive::{Deserialize, Serialize};
 use std::fmt;
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
