96d8478c24fa7988ac13085c4f6f41d349700fcb
Alex Maystrenko <alexeytech@gmail.com>
2020-03-21 21:27:16 +0100

    Reduce maximum number of backups

--- backup-db.sh
+++ backup-db.sh
@@ -6,4 +6,4 @@ fi
 cd "$1"
 echo "Running backup-db in $(pwd)"
 sqlite3 epg.db ".backup epg.db.$(date +%Y-%m-%d)"
-ls |grep 'epg\.db\.' |sort -r |tail -n +5 |xargs -r rm -v
+ls |grep 'epg\.db\.' |sort -r |tail -n +3 |xargs -r rm -v
