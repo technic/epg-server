ae41df6f22052cfe44d3dadcc30699b6d580461e
Alex Maystrenko <alexeytech@gmail.com>
2020-06-01 10:53:15 +0200

    sort js imports

--- web/js/index.js
+++ web/js/index.js
@@ -1,20 +1,20 @@
-import 'bootstrap/dist/js/bootstrap.js'
+import $ from 'jquery'
+import 'bootstrap'
 import 'regenerator-runtime/runtime.js';
 
-import './../style.scss';
-
-import $ from 'jquery'
 import bsCustomFileInput from 'bs-custom-file-input'
 import fromUnixTime from 'date-fns/fromUnixTime'
+import format from 'date-fns/format'
 
 $(document).ready(function () {
     bsCustomFileInput.init()
 
     $("span.date").each(function () {
         const t = $(this).data('timestamp');
-        $(this).text(fromUnixTime(t).format("HH:mm"));
+        $(this).text(format(fromUnixTime(t), "HH:mm"));
     });
 });
 
 import './table.js';
 import './playlist.js';
+import './../index.scss';
